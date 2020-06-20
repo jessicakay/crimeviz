240294637cb716c11898356248f8dec25fa59d29
jessicakay <52629990+jessicakay@users.noreply.github.com>
2020-06-20 12:30:46 -0400

    updates

--- mapit.R
+++ mapit.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+
 # files 
         
 newest_data<-"~/../Downloads/tmpjdulssd7.csv"
@@ -7,14 +9,15 @@ newest_data<-"~/../Downloads/tmpjdulssd7.csv"
 MaAlb_long<-as.numeric("-71.0755771")
 MaAlb_lat<-as.numeric("42.333496")
 geo_zoom<-c(MaAlb_lat,MaAlb_long)
+{
 # create geographic map of crime in Boston
+
 bos_crime<-read.csv(newest_data)
 shootings<-subset(bos_crime,bos_crime$SHOOTING=="Y")
 shots<-table(shootings$DISTRICT,shootings$YEAR)
 years<-as.data.frame(table(shootings$DISTRICT))
 long_incl<-subset(bos_crime,bos_crime$Lat>"-71")
 plot(long_incl$Lat~long_incl$Long)
-library(tidyverse)
 ggplot(long_incl,aes(Long,Lat,color=DISTRICT))+geom_point(alpha=0.1)
 
 # subset homicide
@@ -30,7 +33,7 @@ ggplot(long_incl, aes(Long,Lat,color=ismurder))+
   geom_point(data=hom,alpha=1.0,pch=3)+
   theme(legend.position="right")+
   labs(fill="type of crime")
-
+}
 # create geographic map of auto breakins
 auto<-subset(bos_crime,bos_crime$OFFENSE_CODE_GROUP=="Larceny From Motor Vehicle")
 years<-as.data.frame(table(auto$DISTRICT))
@@ -58,16 +61,17 @@ just2019<-subset(bos_crime,bos_crime$YEAR=="2019")
 
 # turn into function
 
-# create mapping function, plug and play variables
 # spatial landmarks
-# corner of mass ave and albany 
+
+# corner of mass ave and albany, 42.333496, -71.0755771
+
 MaAlb_long<-as.numeric("-71.0755771")
 MaAlb_lat<-as.numeric("42.333496")
 geo_zoom<-c(MaAlb_lat,MaAlb_long)
 
 offCodeGroup<-"Drug Violation"
 offCode<-c(617:700)
-newest_data<-"~/../Downloads/tmpjdulssd7.csv"
+newest_data<-"~/../Downloads/tmpa5mvyouu.csv"
 bos_crime<-read.csv(newest_data)
 
 mapIt<-function(offCode){
@@ -79,9 +83,4 @@ mapIt<-function(offCode){
     scale_color_gradient(low="#deebf7",high="#08306b")+
     geom_hline(yintercept = geo_zoom[1],colour="grey")+
     geom_vline(xintercept = geo_zoom[2],colour="grey")
-  }
-
-just2019<-subset(bos_crime,bos_crime$YEAR=="2019")
-
-# experimental
-ggplot(long_incl,aes(Long,Lat,color=YEAR))+geom_point(alpha=0.1)
+    }
