2710e6899c3787a266a1c1b2767100c5519cd00a
jessicakay <52629990+jessicakay@users.noreply.github.com>
2020-06-27 14:58:29 -0400

    Update threeoneone.R

--- data source scripts/threeoneone.R
+++ data source scripts/threeoneone.R
@@ -3,17 +3,34 @@
 library(sqldf)
 library(tidyverse)
 library(dplyr)
+library(lubridate)
+
+# load data
 
 too<-read.csv("~/../Desktop/tmp_qu8207b.csv")
 colnames(too)
 
+too$open_dt <- as.Date(too$open_dt)
 
+# select out reports with user-submitted photos, dump into dataframe
 
 threeoneone <- too %>% 
                     filter(is.na(submittedphoto)==FALSE) %>%
                     filter(str_detect(submittedphoto,".jpg")) %>%
-                    select(submittedphoto)
+                    filter(open_dt>"2020-06-01") %>%
+                    select(submittedphoto, open_dt)
+
+View(threeoneone)
+
+too_table<-noquote(threeoneone$submittedphoto)
+
+write.table(too_table,"~/../Desktop/threeoneone.txt",
+            row.names = FALSE,
+            quote = FALSE)
+
+# fusker; mass download images
 
 for(i in length(threeoneone)){
   curl::curl_download(url = threeoneone[i])
 }
+
