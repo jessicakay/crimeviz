cb3ee9bbb66389ed5b6e13d07d2aa948c82e2fbb
jessicakay <52629990+jessicakay@users.noreply.github.com>
2020-06-28 09:41:33 -0400

    Update threeoneone.R

--- data source scripts/threeoneone.R
+++ data source scripts/threeoneone.R
@@ -20,10 +20,39 @@ threeoneone <- too %>%
                     filter(open_dt>"2020-06-01") %>%
                     select(submittedphoto, open_dt)
 
+# as a function                    
+
+category<-"Graffiti"
+sub_date<-"2020-05-25"
+end_date<-"2020-06-01"
+targpath<-"~/../Desktop/"
+end_date<-as.Date(end_date)
+
+saveSet<-function(x){
+  write.table(x, noquote(paste(targpath,category,".txt",sep="")),
+              row.names = FALSE,
+              quote = FALSE)}
+
+getSet<-function(category,sub_date,end_date){
+  setList <<- too %>% 
+      filter(reason==category) %>%
+      filter(is.na(submittedphoto)==FALSE) %>%
+      filter(str_detect(submittedphoto,".jpg")) %>%
+      filter(open_dt > as.Date(sub_date)) %>%
+      filter(open_dt < as.Date(end_date)) %>%
+      select(submittedphoto, open_dt, reason)
+  View(setList)
+  setList <<- setList %>% select(submittedphoto)
+  saveSet(setList)
+}
+
+getSet(category,sub_date,end_date)
+
+
+# example query getSet("Graffiti","2020-05-26","2020-05-31")
+
 View(threeoneone)
-
 too_table<-noquote(threeoneone$submittedphoto)
-
 write.table(too_table,"~/../Desktop/threeoneone.txt",
             row.names = FALSE,
             quote = FALSE)
